e01c7d77dd9c6971cda7008dec3f70877f9c756a
MishManners <36594527+MishManners@users.noreply.github.com>
2020-04-07 17:16:25 +1000

    Function files

---
+++ functions.py
@@ -0,0 +1,11 @@
+import sys
+
+def hail_friend(firstfriend):
+    print (f"Hail, {firstfriend} and !")
+
+hail_friend('Jonathan Joestar')
+
+def add_numbers(int1, int2):
+    return int1 + int2
+
+add_numbers(3,7)
---
+++ variables.py
@@ -0,0 +1,3 @@
+favorite_robot = "Cedric"
+
+meaning_of_life = 42
