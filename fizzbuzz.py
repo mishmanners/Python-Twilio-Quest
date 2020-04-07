c851127a47738a4ade4f3a95dad886407017fd5f
MishManners <36594527+MishManners@users.noreply.github.com>
2020-04-07 17:15:06 +1000

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.argv[1])
+# input.pop(0)
+
+# print(input)
+def fizzbuzz(num): n =1
+for n in range(n, num, + 1):
+if (n % 3 == 0) and (n % 5 == 0):
+    print("fizzbuzz")
+elif (n % 3 == 0):
+    print("fizz")
+elif (n % 5 == 0):
+    print("buzz")
+else:
+    print n
