b9aa573a137e6469df20211004af6c7c372e8f16
MishManners <36594527+MishManners@users.noreply.github.com>
2020-04-07 17:15:38 +1000

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,10 @@
+import sys
+
+Glory_of_Python = sys.argv[1]
+
+python_is_glorious = True
+failure_is_option = False
+
+proper_greeting = True
+if sys.argv[1] != "For the glory of Python!":
+    proper_greeting = not proper_greeting
