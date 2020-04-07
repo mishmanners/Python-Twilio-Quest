c472c41586c026c51b9e04fc2d28334494b423ed
MishManners <36594527+MishManners@users.noreply.github.com>
2020-04-07 17:19:56 +1000

    Update fizzbuzz
    Works, found here:
    https://gist.github.com/allengordon011/d3e437711e8702fc395e1194f86409ac

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,20 @@
+def fizzbuzz(num):
+    n = 1
+    for n in range(n, num + 1):
+        if n % 3 == 0 and n % 5 == 0:
+            print 'fizz buzz'
+        elif n % 3 == 0:
+            print 'fizz'
+        elif n % 5 == 0:
+            print 'buzz'
+        else: print n
+
 import sys
 
-n = int(sys.argv[1])
-# input.pop(0)
-
-# print(input)
-def fizzbuzz(num): n =1
-for n in range(n, num, + 1):
-if (n % 3 == 0) and (n % 5 == 0):
-    print("fizzbuzz")
-elif (n % 3 == 0):
-    print("fizz")
-elif (n % 5 == 0):
-    print("buzz")
-else:
-    print n
+for arg in sys.argv[1:]:
+    print len(sys.argv[0:])
+    if len(sys.argv) >= 2:
+        fizzbuzz(int(sys.argv[1]))
+    elif len(sys.argv) < 2:
+        my_input = input('Please enter a number:')
+        fizzbuzz(my_input)
